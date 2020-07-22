245b115096196f0316f88c46d77ad3cb48d0915e
Ning An <ning.an@outlook.com>
2020-07-22 14:05:12 -0400

    Exercise 4

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  .selected {
+    color: red;
+    border-bottom: 2px solid red;
+  }
 `;
 
 export const HeaderSec = () => {
@@ -14,8 +19,12 @@ export const HeaderSec = () => {
       <Header>
         <h1>Fruit Emporium</h1>
         <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          <NavLink exact to="/" activeClassName="selected">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="selected">
+            About
+          </NavLink>
         </div>
       </Header>
     </div>
